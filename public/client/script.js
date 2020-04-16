3ee1bcb4235cc22961a38a13d8bb5229ac913eed
agungardynto <agungardynto@gmail.com>
2020-04-17 06:40:58 +0700

    second commit

--- public/client/script.js
+++ public/client/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io('/');
 
 const getBodyUsers = document.getElementById('body-users');
 const getBodyMessage = document.getElementById('body-message');
